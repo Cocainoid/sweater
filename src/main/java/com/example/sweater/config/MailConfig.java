1450fa18679c1116868b3e46fb80b87a7c913932
Абдрашитов Андрей Васильевич <AVAbdrashitov@sberbank.ru>
2021-06-09 22:00:23 +0300

    Обновил версии плагинов и зависимостей Удалил неиспользуемые зависимости Убрал приватные данные в переменные

--- src/main/java/com/example/sweater/config/MailConfig.java
+++ src/main/java/com/example/sweater/config/MailConfig.java
@@ -3,14 +3,9 @@ package com.example.sweater.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.mail.javamail.MimeMessagePreparator;
 
-import javax.mail.internet.MimeMessage;
-import java.io.InputStream;
 import java.util.Properties;
 
 @Configuration
